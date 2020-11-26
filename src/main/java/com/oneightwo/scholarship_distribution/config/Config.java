33f9a3e7d43a342017dd4d8eeaaa031f321dc72f
oneightwo <37047676+oneightwo@users.noreply.github.com>
2020-11-26 19:36:57 +0400

    + test

--- src/main/java/com/oneightwo/scholarship_distribution/config/Config.java
+++ src/main/java/com/oneightwo/scholarship_distribution/config/Config.java
@@ -2,7 +2,7 @@ package com.oneightwo.scholarship_distribution.config;
 
 public class Config {
 
-//    @Bean
+//    @Beane
 //    public DataSource dataSource() {
 //        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 //        dataSource.setDriverClassName("org.postgresql.Driver");
