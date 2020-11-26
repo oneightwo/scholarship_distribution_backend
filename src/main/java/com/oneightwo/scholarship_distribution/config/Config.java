c796f4e29f5dd233aae798cf456a199c4ad2814d
oneightwo <37047676+oneightwo@users.noreply.github.com>
2020-11-26 19:45:23 +0400

    + dada

--- src/main/java/com/oneightwo/scholarship_distribution/config/Config.java
+++ src/main/java/com/oneightwo/scholarship_distribution/config/Config.java
@@ -8,7 +8,7 @@ er
 //        dataSource.setDriverClassName("org.postgresql.Driver");
 //        dataSource.setUrl("jdbc:postgresql://localhost:5432/scholarship_distribution");
 //        dataSource.setUsername("postgres");
-//        dataSource.setPassword("lumia640");
+//        dataSxource.setPassword("lumia640");
 //        return dataSource;
 //    }
 }
