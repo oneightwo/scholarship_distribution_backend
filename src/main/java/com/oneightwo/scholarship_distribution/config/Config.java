5ac97d2a6ea2e1cc94c3db4c318e29e612f4eead
oneightwo <37047676+oneightwo@users.noreply.github.com>
2020-12-02 20:10:20 +0400

    + b

--- src/main/java/com/oneightwo/scholarship_distribution/config/Config.java
+++ src/main/java/com/oneightwo/scholarship_distribution/config/Config.java
@@ -1,14 +1,6 @@
 package com.oneightwo.scholarship_distribution.config;
 
 public class Config {
-er
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://localhost:5432/scholarship_distribution");
-//        dataSource.setUsername("postgres");
-//        dataSource.setPassword("lumia640");
-//        return dataSource;
-//    }
+
+    тест 1
 }
